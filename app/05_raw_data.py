0f5099e2c618c9ebf7c8e26c14bca58d47701a3e
Stefan Tomic <sttomic@ucdavis.edu>
2024-06-17 16:37:47 -0700

    chore:testing gh actions

--- app/05_raw_data.py
+++ app/05_raw_data.py
@@ -9,6 +9,7 @@ import time
 from snowflake.snowpark import Session
 
 
+
 POS_TABLES = ['country', 'franchise', 'location', 'menu', 'truck', 'order_header', 'order_detail']
 CUSTOMER_TABLES = ['customer_loyalty']
 TABLE_DICT = {
