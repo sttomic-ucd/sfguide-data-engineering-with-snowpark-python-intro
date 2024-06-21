8792439654440e9f00d4c8ed29e14358f2262e54
Stefan Tomic <55515599+sttomic-ucd@users.noreply.github.com>
2024-06-21 23:16:07 +0000

    chore:test gh actions

--- test.py
+++ test.py
@@ -2,4 +2,15 @@ import sys
 import os
 import yaml
 
+ignore_folders = ['.git', '__pycache__', '.ipynb_checkpoints']
+snowflake_project_config_filename = 'snowflake.yml'
+
+if len(sys.argv) != 2:
+    print("Root directory is required")
+    exit()
+
+root_directory = sys.argv[1]
+print(f"Deploying all Snowpark apps in root directory {root_directory}")
+
+
 print("hello world!")
