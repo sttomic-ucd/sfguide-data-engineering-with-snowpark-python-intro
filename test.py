f827efe115d37f5c6e484b53c573f9fe66c45620
Stefan Tomic <55515599+sttomic-ucd@users.noreply.github.com>
2024-06-21 23:27:21 +0000

    chore:test gh actions

--- test.py
+++ test.py
@@ -46,6 +46,8 @@ for (directory_path, directory_names, file_names) in os.walk(root_directory):
         # Make sure all 6 SNOWFLAKE_ environment variables are set
     # SnowCLI accesses the passowrd directly from the SNOWFLAKE_PASSWORD environmnet variable
     os.system(f"snow snowpark build --allow-shared-libraries")
+    os.system(f"snow snowpark deploy --replace --allow-shared-libraries --temporary-connection --account $SNOWFLAKE_ACCOUNT --user $SNOWFLAKE_USER --role $SNOWFLAKE_ROLE --warehouse $SNOWFLAKE_WAREHOUSE --database $SNOWFLAKE_DATABASE")
+
 
 
 print("hello world!")
