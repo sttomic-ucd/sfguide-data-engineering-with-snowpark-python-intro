805e3935607360715a5a9e0e2743a7f1d766bea2
Stefan Tomic <55515599+sttomic-ucd@users.noreply.github.com>
2024-06-22 00:21:09 +0000

    chore:test gh actions

--- test.py
+++ test.py
@@ -48,6 +48,7 @@ for (directory_path, directory_names, file_names) in os.walk(root_directory):
     # os.system(f"snow snowpark build --allow-shared-libraries")
     # # os.system(f"snow snowpark deploy --replace --temporary-connection --account $SNOWFLAKE_ACCOUNT --user $SNOWFLAKE_USER --role $SNOWFLAKE_ROLE --warehouse $SNOWFLAKE_WAREHOUSE --database HOL_DB")
     # os.system(f"snow snowpark deploy --")
+    os.system("ls -l")
     os.system(f'snow connection test --connection="default"')
 
 
