8a61e77a4a77be75d27d92560b4e854de430f8aa
Stefan Tomic <sttomic@ucdavis.edu>
2024-06-24 17:00:38 -0700

    chore:testing gh actions

--- app/test.py
+++ app/test.py
@@ -1,11 +1,4 @@
-from snowflake.snowpark import Session
 
 
-def main(session: Session) -> str:
-    return f"OK"
-    
-# For local debugging
-if __name__ == "__main__":
-    # Create a local Snowpark session
-    with Session.builder.getOrCreate() as session:
-        main(session)
+def main() -> str:
+    return "OK"
